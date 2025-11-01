dd66accd78cdc85c10b217661534f8ba42a69c63
Carlos <grima@MatebookD15.lan>
2025-11-01 15:45:57 +0000

    addition of markBookUnavailable function (instead of deleting it)

--- src/main/java/com/library/manager/LibraryManager.java
+++ src/main/java/com/library/manager/LibraryManager.java
@@ -1,5 +1,72 @@
 package com.library.manager;
 
-public class LibraryManager {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.library.entity.Book;
 
+public class LibraryManager {
+	
+	
+	public void addBook(String title, String isbn, boolean available) {
+		Session session = DatabaseManager.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Book book = new	Book(title, isbn, available);
+		session.persist(book);
+		
+		tx.commit();
+		session.close();
+		
+		System.out.println("Book added: " + title);
+	}
+	
+	// To view all the books (read from the database)
+	
+	public void viewBooks() {
+		Session session =  DatabaseManager.getSessionFactory().openSession();
+		
+		var books = session.createQuery("from book", Book.class).list();
+		
+		System.out.println("\n Book list: ");
+		for (Book b : books) {
+			System.out.println(b);
+		}
+		
+		session.close();
+	}
+		
+	// Delete a book without literally erasing it (mark it as unavailable)
+		
+	public void markBookUnavailable(int id) {
+		Session session = DatabaseManager.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Book book = session.get(Book.class, id);
+		if (book != null) {
+			if (book.getAvailable()) {
+				book.setAvailable(false);
+				session.merge(book);
+				System.out.println("Book marked as unavailable: " + book.getTitle());
+			} else {
+				System.out.println("Book is already unavailable.");
+			}
+		} else {
+			System.out.println("Book not found!");
+		}
+		tx.commit();
+		session.close();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
